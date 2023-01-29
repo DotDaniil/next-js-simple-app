61dc4d650aa9489da3ff75738b202a2b7d65d2f9
DotDaniil <daniil-tikh@mail.ru>
2023-01-29 10:16:59 +0300

    seo optimisation; main problem what next.js solve

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
 import A from "../components/A";
+import Head from "next/head";
 
 const Index = () => {
     return (
-        <div>
-            <div className="navbar">
-                <A href={"/"} text="Главная"/>
-                <A href={"/users"} text="Пользователи"/>
+        <>
+            <Head>
+                <meta keywords="DotDaniil, next.js"></meta>
+                <title>Главная страница</title>
+            </Head>
+            <div>
+                <div className="navbar">
+                    <A href={"/"} text="Главная"/>
+                    <A href={"/users"} text="Пользователи"/>
+                </div>
+                <h1>Главная страница</h1>
+                <style jsx>
+                    {`
+                      .navbar {
+                        background: orange;
+                        padding: 15px;
+                      }
+                    `}
+                </style>
             </div>
-            <h1>Главная страница</h1>
-            <style jsx>
-                {`
-                  .navbar {
-                    background: orange;
-                    padding: 15px;
-                  }
-                `}
-            </style>
-        </div>
+        </>
     );
 };
 
